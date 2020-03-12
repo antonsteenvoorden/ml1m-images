3a2fddf934f7f21b0646fd8a76a90ed6f8a0082e
Anton S <antons@live.nl>
2020-03-12 17:39:01 +0100

    clean up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ml1m images
 
-This repository provides researchers with URLs to each movie from the ![MovieLens-1M data set](https://grouplens.org/datasets/movielens/1m)  
+This repository provides researchers with URLs to each movie from the [MovieLens-1M data set](https://grouplens.org/datasets/movielens/1m)  
 
 This can be easily used together with the existing information provided by merging the two CSVs on the item_id column:  
 ```
@@ -9,5 +9,5 @@ ml1m_images = pd.read_csv("ml1m_images.csv")
 other_df_with_the_ids.merge(ml1m_images, on="item_id", how="left")
 ```
 
-The script provides a way to automatically download all images, named accordingly `item_id.jpg`.
+The script provides a way to automatically download all images, named accordingly `<item_id>.jpg`.
 
--- get_images.py
+++ get_images.py
@@ -1,20 +1,14 @@
 import os
-import shutil
 import pandas as pd
 import urllib.request
-import urllib.parse
-import json
-import time
 
 FOLDER = "images"
-ORIG_FOLDER = "images_orig_id"
 INPUT_FILE = "movie_images.csv"
 
 
 def download_image(df):
     new_id, item_id, image = df
-    image_path = f"{FOLDER}/{new_id}.jpg"
-    orig_image_path = f"{ORIG_FOLDER}/{item_id}.jpg"
+    image_path = f"{FOLDER}/{item_id}.jpg"
 
     if image == "" or image is None or not isinstance(image, str):
         print("Failed: id", new_id, image)
@@ -25,13 +19,11 @@ def download_image(df):
 
     try:
         urllib.request.urlretrieve(image, image_path)
-        shutil.copyfile(image_path, orig_image_path)
     except Exception:
         print("ERROR: id", new_id, image)
 
 
 if __name__ == '__main__':
     os.makedirs(FOLDER, exist_ok=True)
-    os.makedirs(ORIG_FOLDER, exist_ok=True)
-    merged = pd.read_csv(INPUT_FILE)
-    merged[['new_id', 'item_id', 'image']].apply(lambda x: download_image(x), axis=1)
+    df = pd.read_csv(INPUT_FILE)
+    df[['new_id', 'item_id', 'image']].apply(lambda x: download_image(x), axis=1)
